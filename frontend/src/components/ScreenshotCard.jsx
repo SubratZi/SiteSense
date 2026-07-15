df613fe1f678490fca0067d4e830613920350058
subratzi <167219761+SubratZi@users.noreply.github.com>
2026-07-15 07:31:14 +0545

    update: enhanced UI

--- frontend/src/components/ScreenshotCard.jsx
+++ frontend/src/components/ScreenshotCard.jsx
@@ -2,22 +2,12 @@ function ScreenshotCard({screenshot}) {
     if(!screenshot) return null;
 
     return (
-        <div
-            style={{
-                border: "1px solid #ddd",
-                borderRadius: "10px",
-                padding: "20px",
-                marginTop: "20px",
-            }}
-        >
-            <h2>Website Screenshot</h2>
-            <img src={`http://127.0.0.1:8000/${screenshot}`}
-                alt = "Website Screenshot"
-                style={{
-                    width: "100%",
-                    borderRadius: "8px",
-                    border: "1px solid #ccc",
-                }} 
+        <div className = "card">
+            <p className = "card-title"> Screenshot</p>
+            <img 
+                className = "screenshot-img" 
+                src={`http://127.0.0.1:8000/${screenshot}`}
+                alt = "Website Screenshot" 
             />
         </div>
     );
