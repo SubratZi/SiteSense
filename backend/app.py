61b7f6c58b87bb5be40f74764fd48f393a365a2a
subratzi <167219761+SubratZi@users.noreply.github.com>
2026-07-15 08:48:00 +0545

    chore : updated app

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins = [
         "http://localhost:5173",
-        "sitesenseseo.vercel.app",
+        "https://sitesenseseo.vercel.app",
     ],
     allow_credentials = True,
     allow_methods = ["*"],
